b2d280a50a11d003bb84e2220f446e1f38517ee2
Ben Theunissen <ben.theunissen@rea-group.com>
2019-05-01 11:25:32 +1000

    Add kotlin test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,13 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+group = "org.theunissen.tech"
+version = "1.0-SNAPSHOT"
+
+val main_class by extra("AppKt")
 
 plugins {
     application
     kotlin("jvm") version "1.3.31"
-    id("com.google.cloud.tools.jib") version "1.1.2"
+    id("com.google.cloud.tools.jib").version("1.1.2")
 }
-
-group = "org.theunissen.tech"
-version = "1.0-SNAPSHOT"
-
-val arrow_version = "0.9.0"
-
-val main_class by extra("AppKt")
 
 application {
     mainClassName = main_class
@@ -35,9 +31,19 @@ repositories {
     maven( "https://oss.jfrog.org/artifactory/oss-snapshot-local/")
 }
 
+val kotlin_version = "1.3.31"
+val arrow_version = "0.9.0"
+val kotlintest_version = "3.3.2"
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     compile("io.arrow-kt:arrow-core-data:$arrow_version")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:$kotlintest_version")
+    testImplementation("io.kotlintest:kotlintest-assertions-arrow:$kotlintest_version")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 jib {
